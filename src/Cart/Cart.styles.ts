0737c9d7eb7d0b7c4f0f37bca6e14c27ff6d0651
YASWANTH KANJARLA <71338071+yash-phoenix79@users.noreply.github.com>
2023-09-13 17:01:50 +0530

    Add files via upload

---
+++ src/Cart/Cart.styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Wrapper= styled.aside`
+
+font-family: Arial,Helvetica,sans-serif;
+@media
+
+padding: 20px;
+
+
+@media (max-width: 767px) {
+width: 67vw;
+padding: 20px;
+}
+
+@media (min-width: 768px)  {
+width:50vw;    
+padding: 20px;
+}
+
+
+
+`
